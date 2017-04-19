aadc0f8890829752fa0ac5636fe40856819ffa7a
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-04-19 00:04:56 -0600

    successfully making nested ajax call

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,30 +1,28 @@
 $(document).ready(function() {
-  $.ajax({
-    method: "GET",
-    url: "https://www.humanitarianresponse.info/api/v1.0/disasters?filter[status]=current",
-  })
-  .then(function( disasters ) {
-    console.log( disasters );
-    console.log( disasters["data"].length );
-
-    var disasterString = ""
-    disasters["data"].forEach ( function(disaster){
-      disasterString = disasterString + formatDisaster(disaster)
-      disaster["operation"].forEach ( function(operation){
-        var test = function( ) {
-          $.ajax({
-          method: "GET",
-          url: "https://www.humanitarianresponse.info/api/v1.0/disasters?filter[status]=current",
-        })
-      }.then(function)(operation) {
-        console.log( operation );
-      }
-      })
-    } )
-
-    $("#name").append( disasterString );
-  });
+  ajaxCall1();
 })
+
+function ajaxCall1() {
+  $.ajax({
+        method: "GET",
+        url: "https://www.humanitarianresponse.info/api/v1.0/disasters?filter[status]=current",
+        })
+    .then(function( disasters ) {
+      console.log( disasters );
+      fetchDisasters(disasters);
+      $("#name").append( disasterString );
+  })
+}
+var disasterString = ""
+
+function fetchDisasters(disasters) {
+  disasters["data"].forEach (function(disaster) {
+    disasterString = disasterString + formatDisaster(disaster)
+    if (disaster["operation"]) {
+      fetchCoordinates(disaster["operation"])
+    }
+  })
+}
 
 function formatDisaster(disaster) {
   return "<li>Name: "
@@ -35,3 +33,17 @@ function formatDisaster(disaster) {
   + disaster["primary_type"]
   + "</li></ul>"
 }
+
+function fetchCoordinates(operations) {
+      operations.forEach (function(operation) {
+        console.log( operation );
+        $.ajax({
+        	method: "GET",
+        	url: operation.self
+        })
+        .then(function(coordinates){
+          console.log(coordinates["data"][0]["country"]["geolocation"])
+          debugger
+        })
+      })
+  }
