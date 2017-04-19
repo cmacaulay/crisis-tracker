e29eddae203ac9b259df0062df8de4f46f689f90
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-04-18 21:59:00 -0600

    add functioning js back in

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
-  // Call functions
-
-  fetchDisasters();
-  createDisasterList();
-
-function fetchDisasters() {
   $.ajax({
     method: "GET",
     url: "https://www.humanitarianresponse.info/api/v1.0/disasters?filter[status]=current",
-  }).then(function( disasters ))  
-}
-
+  })
   .done(function( disasters ) {
     console.log( disasters );
     console.log( disasters );
@@ -18,18 +10,17 @@ function fetchDisasters() {
     var disasterString = ""
     disasters["data"].forEach ( function(disaster){
       disasterString = disasterString + formatDisaster(disaster)
-      disaster["operation"].forEach ( function(operation){
-        debugger
-        $.ajax({
-          method: "GET",
-          url: operation["self"],
-        })
-      })
     } )
     $("#name").append( disasterString );
   });
 })
 
 function formatDisaster(disaster) {
-  return "<li>Name: " + disaster["label"] + "</li>\n<ul><li>Description: " + disaster["body-html"] + "</li><li>Type: " + disaster["primary_type"] + "</li></ul>"
+  return "<li>Name: "
+  + disaster["label"]
+  + "</li>\n<ul><li>Description: "
+  + disaster["body-html"]
+  + "</li><li>Type: "
+  + disaster["primary_type"]
+  + "</li></ul>"
 }
